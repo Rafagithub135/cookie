ec9bc78736b8ba0a232c5b67c02284f9d9011139
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-03-27 14:22:36 -0400

    adding example properties

--- src/main/java/com/example/cookie/CookieApplication.java
+++ src/main/java/com/example/cookie/CookieApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CookieApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CookieApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CookieApplication.class, args);
+    }
 
 }
