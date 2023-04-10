ec07b037623ac15d2f342d5bee868d8acafd9579
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-04-10 18:35:38 -0400

    added a view page and modified some code

--- src/main/java/com/example/cookie/controllers/EventController.java
+++ src/main/java/com/example/cookie/controllers/EventController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class BookController {
-    @GetMapping("/book")
+public class EventController {
+    @GetMapping("/events/book")
     public String book() {
-        return "book/index";
+        return "events/book";
     }
 }
