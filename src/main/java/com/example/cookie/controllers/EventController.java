2edbe78b11851ef6e10e703f7d06f452f56b9997
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-04-13 21:58:30 -0400

    added eventcontroller but commented code until I can sort it out

--- src/main/java/com/example/cookie/controllers/EventController.java
+++ src/main/java/com/example/cookie/controllers/EventController.java
@@ -8,35 +8,35 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@Controller
-public class EventController {
-    private final EventRepository eventDao;
-
-    public EventController(EventRepository eventDao) {
-        this.eventDao = eventDao;
-    }
-
-    @GetMapping("/events")
-    public String eventsHome(Model model) {
-        List<Event> events = eventDao.findAll();
-        model.addAttribute("events", events);
-        return "events/upcoming";
-    }
-
-    @GetMapping("/events/book")
-    public String book() {
-        return "events/book";
-    }
-
-    @GetMapping("/events/{id}")
-    public String postsView(@PathVariable long id, Model model) {
-        Event event = eventDao.findEventById(id);
-        model.addAttribute("event", event);
-        return "events/upcoming";
-    }
-
-    @GetMapping("/events/upcoming")
-    public String list() {
-        return "events/upcoming";
-    }
-}
+//@Controller
+//public class EventController {
+//    private final EventRepository eventDao;
+//
+//    public EventController(EventRepository eventDao) {
+//        this.eventDao = eventDao;
+//    }
+//
+//    @GetMapping("/events")
+//    public String eventsHome(Model model) {
+//        List<Event> events = eventDao.findAll();
+//        model.addAttribute("events", events);
+//        return "events/upcoming";
+//    }
+//
+//    @GetMapping("/events/book")
+//    public String book() {
+//        return "events/book";
+//    }
+//
+//    @GetMapping("/events/{id}")
+//    public String postsView(@PathVariable long id, Model model) {
+//        Event event = eventDao.findEventById(id);
+//        model.addAttribute("event", event);
+//        return "events/upcoming";
+//    }
+//
+//    @GetMapping("/events/upcoming")
+//    public String list() {
+//        return "events/upcoming";
+//    }
+//}
